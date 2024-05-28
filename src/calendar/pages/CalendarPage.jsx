5f3ccfacfe5a8210298db1d07fae9476aa70aa3e
ivansaavedra2097 <ivan.saavedra.iezs@gmail.com>
2024-05-28 15:27:33 -0600

    agregar calendario react-big-calendar

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,10 +1,50 @@
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+
+// import format from 'date-fns/format'
+// import parse from 'date-fns/parse'
+// import startOfWeek from 'date-fns/startOfWeek'
+// import getDay from 'date-fns/getDay'
+import enUS from 'date-fns/locale/en-US'
+import { addHours, parse, startOfWeek, getDay, format } from 'date-fns'
+
 import { Navbar } from "../"
+
+const locales = {
+  'en-US': enUS,
+}
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+})
+
+const events = [{
+  title: 'my wedding',
+  notes: 'buy the ring',
+  start: new Date(),
+  end: addHours( new Date(), 2),
+  bgColor: '#fafafa',
+  user: {
+    _id: 'asdf3332',
+    name: 'Ivan'
+  }
+}]
 
 export const CalendarPage = () => {
   return (
     <>
       <Navbar />
-      <div>CalendarPage</div>
+      <Calendar
+        localizer={ localizer }
+        events={ events }
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 'calc(100vh - 80px)' }}
+      />
     </>
   )
 }
